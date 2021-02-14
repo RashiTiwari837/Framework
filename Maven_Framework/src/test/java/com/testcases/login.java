eab3f4cd9925b476671c7990150853e24ab8b24a
Rashi <Rashi@10.0.50.164>
2021-02-14 19:21:34 +0530

    new comment added

--- Maven_Framework/src/test/java/com/testcases/login.java
+++ Maven_Framework/src/test/java/com/testcases/login.java
@@ -18,7 +18,7 @@ import com.utilities.Helper;
 
 public class login extends baseclass {
 	
-	 
+	 // this is new commit
 			@Test
 			public void loginapp() throws InterruptedException, Exception {
 				
